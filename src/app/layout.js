c3b7e0adf5fc4e904099dd1a222807a5137d1f8e
PremerX007 <bannawat_55120@outlook.com>
2023-09-28 15:23:18 +0700

    completed

--- src/app/layout.js
+++ src/app/layout.js
@@ -91,8 +91,8 @@ export default function RootLayout({ children }) {
               </Title>
               {children}
               <Footer
-                studentId="650610560"
-                fullName="Chayanin Suatap"
+                studentId="650610778"
+                fullName="Bannawat Vongpooton"
                 year="2023"
               />
             </Container>
